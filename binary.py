bcbd6121de6e730d2ab4199adedf501a06a29e9f
clarasalless <95931755+clarasalless@users.noreply.github.com>
2023-09-18 15:46:27 -0300

    arquivos

--- binary.py
+++ binary.py
@@ -41,19 +41,9 @@ def build_header(header_ver, header_valid, prod_id, prod_version, length):
 def parse_srec_line(line):
     record_type = line[0:2]
     data_length = int(line[2:4], 16)
-    address = line[4:8]
+    address = int(line[4:8], 16)
     data = line[8:-2].decode('utf-8')
     checksum = line[-2:]
-
-    # Preenche as linhas caso o dado seja menor que 16 bytes
-    if len(data) < 32:
-        for i in range(32-len(data)):
-            data = data + 'F'
-
-    print(data)
-
-    # transforma o dado em bytearray
-    data = bytearray.fromhex(data)
 
     return {
         "record_type": record_type,
@@ -63,34 +53,74 @@ def parse_srec_line(line):
         "checksum": checksum
     }
 
+# completa a string de dados quando o tamanho não é multiplo de 64
 
-# lê um arquivo .mot e armazena as informações de uma seção limitada por dois endereços
-def binary_gen(destination_path, file_path, start_address, end_address):
-    records = []
-    within_range = False
 
-    with open(destination_path, "wb") as destination:
-        with open(file_path, "rb") as srec:
-            for line in srec:
+def mul64(data):
+    length = len(data)/2
+    if length % 64 == 0:
+        return data
+    else:
+        return data + int(64-(length % 64)) * 'FF'
+
+
+def binary_gen(destination_path, file_path):
+    code1 = ''  # string que contém a primera parte do código
+    code2 = ''  # string que contém a segunda parte do código
+    end_address = 0  # guarda o último endereço preenchido
+    previous_end_address = 0  # guarda o último endereço preenchido na iteração anterior
+
+    with open(destination_path, 'wb') as destination:
+        with open(file_path, 'rb') as mot:
+            for line in mot:
                 line = line.strip()
                 record = parse_srec_line(line)
-                if record["record_type"] == b'S1':
-                    if int(record["address"], 16) >= start_address and int(record["address"], 16) <= end_address:
-                        within_range = True
-                        records.append(record)
-                        destination.write(record["data"])
-                    elif within_range and int(record["address"], 16) > end_address:
-                        break
 
-    return records
+                # endereço inicial do dado
+                init_address = record['address']
+                # endereço final do dado
+                end_address = record['address'] + record['data_length'] - 3
+
+                # verifica se a linha pertence à primeira parte
+                if record['record_type'] == b'S1' and record['address'] < int(0xFFF):
+
+                    # preenche bytes vazios quando o endereço do início da linha é maior que o endereço do fim da linha anterior
+                    if previous_end_address < init_address:
+                        code1 += (init_address-previous_end_address)*'FF'
+
+                    # junta o dado à primeira string
+                    code1 += record["data"]
+
+                # verifica se a linha pertence à segunda parte
+                elif record['record_type'] == b'S1' and record['address'] >= int(0x3000):
+
+                    # se a linha for a primeira da segunda parte, altera o valor do endereço final da linha anterior
+                    if record['address'] == int(0x3000):
+                        previous_end_address = 0x3000
+
+                    # preenche bytes vazios quando o endereço do início da linha é maior que o endereço do fim da linha anterior
+                    if previous_end_address < init_address:
+                        code2 += (init_address-previous_end_address)*'FF'
+
+                    # junta o dado à segunda string
+                    code2 += record["data"]
+
+                # atualiza o endereço final anterior
+                previous_end_address = end_address
+
+        # completando os códigos para que o tamanho seja múltiplo de 64
+        code1 = mul64(code1)
+        code2 = mul64(code2)
+
+        # escrevendo o arquivo binário
+        code1_size = len(bytearray.fromhex(code1))
+        code2_size = len(bytearray.fromhex(code2))
+        binary_data = bytearray.fromhex(code1 + code2)
+        destination.write(binary_data)
 
 
 # testando
 destination_path = r'Arquivos WPS\comparar.bin'
 file_path = r"Arquivos WPS\rl_application.mot"
-start_address = 0x0000
-end_address = 0x0FFF
 srec_records = binary_gen(
-    destination_path, file_path, start_address, end_address)
-
-print(srec_records)
+    destination_path, file_path)
