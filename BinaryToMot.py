dfc8ab895534d8ad1341a58ae07cd7d0a6e5bfbd
clarasalless <95931755+clarasalless@users.noreply.github.com>
2024-01-23 14:58:57 -0300

    executavel do rx e função para dividir os rl

--- BinaryToMot.py
+++ BinaryToMot.py
@@ -215,6 +215,15 @@ def mot_to_binary_rl(file_path):
     return app, vector_table
 
 
+def rl_division(app, vector_table):
+
+    all_data = vector_table['data'] + app['data']
+    part1 = all_data[:vector_table['size'] * 2]
+    part2 = all_data[vector_table['size'] * 2:]
+
+    return part1, part2
+
+
 # Função: build_header
 # Parâmetros de Entrada: header (dicionário com informações do cabeçalho)
 # Saída: Dados do cabeçalho empacotados
@@ -325,6 +334,16 @@ def mot_gen(destination_path, mot_list):
         destination.write(all_data)
 
 
+mot_path = r"F:\CORPORATE_MOT_FINAL\231215A_IHM_8.mot"
+app, vec = mot_to_binary_rl(mot_path)
+# print(ascii_to_mot(app['data'], 0x00))
+# print()
+# print()
+# print(ascii_to_mot(vec['data'], 0x00))
+
+part1, part2 = rl_division(app, vec)
+print(ascii_to_mot(part1, 0x00))
+print(ascii_to_mot(part2, 0x00))
 
 
 
